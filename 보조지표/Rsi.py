1e822a76bbad4edafd9dec957a3e77577c474689
15sec <dailylogle@gmail.com>
2022-01-01 11:25:57 +0900

    macd 추가 / 불필요한코드제거

--- 보조지표/Rsi.py
+++ 보조지표/Rsi.py
@@ -3,11 +3,10 @@ import backtrader
 
 
 class Rsi(backtrader.Strategy):
-    params = dict(period=20, devfactor=2)
 
     def __init__(self):
         self.order = None
-        self.rsi = backtrader.ind.RSI_SMA(self.data.close, period=21)
+        self.rsi = backtrader.ind.RSI_SMA(self.data.close, period=14)
 
     def next(self):
         if not self.position:
@@ -30,7 +29,7 @@ class Rsi(backtrader.Strategy):
             self.order = None
             return
 
-    def log(self, txt, dt=None):
+    def log(self, txt):
         dt = self.datas[0].datetime.date(0)
         print('[{0}] {1}'.format(dt.isoformat(), txt))
 
@@ -38,17 +37,17 @@ class Rsi(backtrader.Strategy):
 if __name__ == '__main__':
     cerebro = backtrader.Cerebro()
 
-    # 2018년 5월 4일(액면 분할 이후)부터 2021년 9월까지 삼성전자 데이터
+    # 2018년 5월 4일(액면 분할 이후)부터 2021년 12월까지 삼성전자 데이터
     data = backtrader.feeds.YahooFinanceData(dataname='005930.KS',
-                                             fromdate=datetime(2018, 5, 4), todate=datetime(2021, 12, 1))
+                                             fromdate=datetime(2021, 1, 1), todate=datetime(2021, 12, 31))
 
     cerebro.adddata(data)
     cerebro.addstrategy(Rsi)
     cerebro.broker.setcash(10000000)  # 초기자금 1000만원
     cerebro.broker.setcommission(commission=0.0014)  # 매수/매도시 나가므로 수수료 0.28의 절반
-    cerebro.addsizer(backtrader.sizers.PercentSizer, percents=49)  # 비중 49%
-    print('초기자산: {0:,.0f}원 (매수 비중: {1:,.0f}%)'.format(cerebro.broker.getvalue(), 49))
+    cerebro.addsizer(backtrader.sizers.PercentSizer, percents=98)  # 비중 49%
+    print('초기자산: {0:,.0f}원 (매수 비중: {1:,.0f}%)'.format(cerebro.broker.getvalue(), 98))
     cerebro.run()
     percent = ((cerebro.broker.getvalue() / 10000000) - 1.0) * 100
     print('최종자산: {0:,.0f}원(수익률: {1:,.2f}%)'.format(cerebro.broker.getvalue(), percent))
-    #cerebro.plot(style='candlestick')
+    # cerebro.plot(style='candlestick')
--- 보조지표/macd.py
+++ 보조지표/macd.py
@@ -2,18 +2,19 @@ from datetime import datetime
 import backtrader
 
 
-class Ich(backtrader.Strategy):
+class MACD(backtrader.Strategy):
 
     def __init__(self):
         self.order = None
-        self.ichimoku = backtrader.ind.Ichimoku()
+        self.macd = backtrader.ind.MACD()
+        self.crossover = backtrader.ind.CrossOver(self.macd.macd, self.macd.signal)
 
     def next(self):
         if not self.position:
-            if self.ichimoku.tenkan_sen > self.ichimoku.kijun_sen:
+            if self.crossover > 0:
                 self.buy()
         else:
-            if self.ichimoku.tenkan_sen < self.ichimoku.kijun_sen:
+            if self.crossover < 0:
                 self.sell()
 
     def notify_order(self, order):
@@ -29,7 +30,7 @@ class Ich(backtrader.Strategy):
             self.order = None
             return
 
-    def log(self, txt, dt=None):
+    def log(self, txt):
         dt = self.datas[0].datetime.date(0)
         print('[{0}] {1}'.format(dt.isoformat(), txt))
 
@@ -39,14 +40,15 @@ if __name__ == '__main__':
 
     # 2018년 5월 4일(액면 분할 이후)부터 2021년 12월까지 삼성전자 데이터
     data = backtrader.feeds.YahooFinanceData(dataname='005930.KS',
-                                             fromdate=datetime(2018, 5, 4), todate=datetime(2021, 12, 1))
+                                             fromdate=datetime(2018, 5, 4), todate=datetime(2021, 12, 31))
+
     cerebro.adddata(data)
-    cerebro.addstrategy(Ich)
+    cerebro.addstrategy(MACD)
     cerebro.broker.setcash(10000000)  # 초기자금 1000만원
     cerebro.broker.setcommission(commission=0.0014)  # 매수/매도시 나가므로 수수료 0.28의 절반
-    cerebro.addsizer(backtrader.sizers.PercentSizer, percents=49)  # 비중 49%
-    print('초기자산: {0:,.0f}원 (매수 비중: {1:,.0f}%)'.format(cerebro.broker.getvalue(), 49))
+    cerebro.addsizer(backtrader.sizers.PercentSizer, percents=98)  # 비중 98%
+    print('초기자산: {0:,.0f}원 (매수 비중: {1:,.0f}%)'.format(cerebro.broker.getvalue(), 98))
     cerebro.run()
     percent = ((cerebro.broker.getvalue() / 10000000) - 1.0) * 100
     print('최종자산: {0:,.0f}원(수익률: {1:,.2f}%)'.format(cerebro.broker.getvalue(), percent))
-    #cerebro.plot(style='candlestick')
+    cerebro.plot(style='candlestick')
